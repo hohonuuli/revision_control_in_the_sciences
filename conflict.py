9297d5d5968706f415f5cfefac06e1b7217bcab6
Brandon W. King <kingb@berkeley.edu>
2013-08-08 13:44:22 -0700

    Attempting to create conflict.

--- conflict.py
+++ conflict.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 
-def conflict(x,y):
+def create_conflict(x,y):
   return x + y
 
-
 if __name__ == '__main__':
-  result = conflict(2,3)
+  result = create_conflict(2,3)
   print('Result: {0}'.format(result))
